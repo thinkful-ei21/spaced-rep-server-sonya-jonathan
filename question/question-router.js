6fba8a7db93b7b791adba6b90caa2cacf533b254
BuddhaL1T3 <jmriggs75@gmail.com>
2018-08-09 10:08:16 -0400

    refactored

--- question/question-router.js
+++ question/question-router.js
@@ -41,13 +41,13 @@ router.post('/', (req, res, next) => {
       const currIndex = user.head;
 
       // if user gives correct answer, increase score and multipy mValue by 2
-      if (currQuestion.answer === userAnswer) {
+      if (currQuestion.answer === userAnswer.toUpperCase()) {
         feedback = true;
         currQuestion.numCorrect++;
         currQuestion.numAttempts++;
         currQuestion.mValue *= 2;
       } else {
-      // if user gives incorrect answer increase attempts and reset mValue to 1
+        // if user gives incorrect answer increase attempts and reset mValue to 1
         feedback = false;
         currQuestion.numAttempts++;
         currQuestion.mValue = 1;
@@ -58,7 +58,7 @@ router.post('/', (req, res, next) => {
       let insertAfterQuestion = currQuestion;
       let currQ = currQuestion;
 
-      // loop through questions to find the question object at the index that 
+      // loop through questions to find the question object at the index that
       // matches the mValue of the current question
       // set insertAfterQuestion to equal the question at the mValue index
       for (let i = 0; i < currQuestion.mValue; i++) {
@@ -74,7 +74,7 @@ router.post('/', (req, res, next) => {
       // if the insertion point is at the end, make the currQuestion point to null
       if (insertAfterQuestion.next === null) {
         currQuestion.next = null;
-      // otherwise set the currentQuestion to point to the node after the insertion point
+        // otherwise set the currentQuestion to point to the node after the insertion point
       } else {
         currQuestion.next = insertAfterQuestion.next;
       }
